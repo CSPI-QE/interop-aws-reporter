7da365606f34ecac26d8d34778e700ad8e7e0aa0
oharan2 <oharan@redhat.com>
2024-11-20 18:56:05 +0200

    Prepare to run as POC in Openshift-Ci env

--- aws_reporter/app.py
+++ aws_reporter/app.py
@@ -1,37 +1,13 @@
 import os
 from typing import Any
-import json
-import requests
 from simple_logger.logger import get_logger
+from utils import send_slack_message
 
 logger = get_logger(name=__name__)
 
-# docker build -t aws_reporter .
-# docker run -p 8000:8000 --env-file .development/.env aws_reporter:latest
-# docker run -it -p 8000:8000 --env-file .development/.env aws_reporter:latest /bin/bash
-
-
-def send_slack_message(message: str, webhook_url: str, logger: Any) -> None:
-    try:
-        if webhook_url:
-            slack_data = {"text": message}
-            logger.info(f"Sending message to slack: {message}")
-            response = requests.post(
-                webhook_url,
-                data=json.dumps(slack_data),
-                headers={"Content-Type": "application/json"},
-            )
-            if response.status_code != 200:
-                logger.error(
-                    f"Request to slack returned an error {response.status_code} with the following message: {response.text}"
-                )
-            logger.info(f"Slack message sent successfully: {response.text}")
-    except Exception as ex:
-        logger.error(f"Failed to send slack message. error: {ex}")
-
 
 def aws_report() -> Any:
-    message = "Hello :wave:, here is the weekly run report for [...] .\nAWS Reporting is cool from a container!"
+    message = "Hello :wave:, here is the weekly run report for [...] .\nAWS Reporting is cool! (sent from a container)"
     slack_webhook_url = os.environ.get("SLACK_WEBHOOK_URL")
     if not slack_webhook_url:
         return {"error": "slack_errors_webhook_url is required"}, 400
@@ -47,5 +23,4 @@ def aws_report() -> Any:
 
 if __name__ == "__main__":
     # dry_run(logger)
-    # aws_report()
-    print("Cool App")
+    aws_report()
