6cb13fc5ad1aab1136005d60c9ff813569b48f02
oharan2 <oharan@redhat.com>
2024-12-05 12:57:32 +0200

    Use dry_run to send Slack message

--- interop_aws_reporter/app.py
+++ interop_aws_reporter/app.py
@@ -1,27 +1,9 @@
-import os
-from typing import Any
 from simple_logger.logger import get_logger
-from utils import send_slack_message
+from log_aws_report import dry_run
 
 logger = get_logger(name=__name__)
 
 
-def aws_report() -> Any:
-    message = "Hello :wave:, here is the weekly run report for [...] .\nAWS Reporting is cool! (sent from Openshift-Ci)"
-    slack_webhook_url = os.environ.get("SLACK_WEBHOOK_URL")
-    if not slack_webhook_url:
-        logger.error("Missing env var. SLACK_WEBHOOK_URL is required.")
-        raise
-
-    if slack_webhook_url:
-        send_slack_message(
-            message=message,
-            webhook_url=slack_webhook_url,
-            logger=logger,
-        )
-    logger.info("Message sent")
-
-
 if __name__ == "__main__":
-    # dry_run(logger)
-    aws_report()
+    dry_run(logger=logger)
+    # aws_report()
--- interop_aws_reporter/log_aws_report.py
+++ interop_aws_reporter/log_aws_report.py
@@ -1,8 +1,29 @@
 import subprocess
 import tempfile
 from typing import Any
+from utils import send_slack_message
+import os
 
 AWS_CLIENT_REGION = "us-east-1"
+
+
+def aws_report(logger: Any, message: str = "") -> None:
+    if not message:
+        message = (
+            "Hello :wave:, here is the weekly run report for [...] .\nAWS Reporting is cool! (sent from Openshift-Ci)"
+        )
+    slack_webhook_url = os.environ.get("SLACK_WEBHOOK_URL")
+    if not slack_webhook_url:
+        logger.error("Missing env var. SLACK_WEBHOOK_URL is required.")
+        raise
+
+    if slack_webhook_url:
+        send_slack_message(
+            message=message,
+            webhook_url=slack_webhook_url,
+            logger=logger,
+        )
+    logger.info("Message sent")
 
 
 def dry_run(logger: Any) -> None:
@@ -15,14 +36,18 @@ def dry_run(logger: Any) -> None:
 
     cloudwash_output = subprocess.run(["poetry", "run", "swach", "--help"], capture_output=True, text=True)
 
-    logger.info(cloudwash_output.stdout)
-
     if cloudwash_output.returncode != 0:
         raise Exception(f"CloudWash execution failed: {cloudwash_output.stderr}")
     else:
+        swach_output = cloudwash_output.stdout
+
+        logger.info(swach_output)
+
         # Create a temporary file to store the logs
         with tempfile.NamedTemporaryFile(delete=False) as tmpfile:
-            tmpfile.write(cloudwash_output.stdout.encode())
+            tmpfile.write(swach_output.encode())
             tmpfile_path = tmpfile.name
 
-            logger.info(f"Output saved to: {tmpfile_path}")
+            message = f"Output saved to: {tmpfile_path}"
+            logger.info(message)
+            aws_report(logger=logger, message=message)
